824125efa2d95371e946f5b446aab26162c109b1
jeromerg <jeromerg@gmx.net>
2015-09-21 17:04:00 +0200

    cleaning

--- src/NCase.Core/Api/Dev/Core/Parse/IAddNodeDirector.cs
+++
@@ -1,9 +0,0 @@
-using NDsl.Api.Core.Nod;
-using NVisitor.Api.PairBatch;
-
-namespace NCase.Api.Dev.Core.Parse
-{
-    public interface IAddNodeDirector : IPairDirector<INode /*existing set*/, INode /*node to add*/, IAddNodeDirector>
-    {
-    }
-}
