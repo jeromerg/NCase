3add887506a140c1d7ab9afc15c27cac44e84134
jeromerg <jeromerg@gmx.net>
2015-09-21 14:21:44 +0200

    Change formatting file layout: public static methods after ctors, other static methods at the end

--- src/NCase.Core/Imp/Pairwise/DumpVisitors.cs
+++ src/NCase.Core/Imp/Pairwise/DumpVisitors.cs
@@ -10,17 +10,17 @@ namespace NCase.Imp.Pairwise
     public class DumpVisitors
         : IActionVisitor<INode, IDumpDirector, IPairwiseNode>
     {
+        public void Visit(IDumpDirector dir, IPairwiseNode node)
+        {
+            dir.AddText("Pairwise: {0}", node);
+            VisitNextLevel(dir, node.Children);
+        }
+
         private static void VisitNextLevel(IDumpDirector dir, IEnumerable<INode> children)
         {
             dir.Indent();
             children.ForEach(c => dir.Visit(c));
             dir.Dedent();
-        }
-
-        public void Visit(IDumpDirector dir, IPairwiseNode node)
-        {
-            dir.AddText("Pairwise: {0}", node);
-            VisitNextLevel(dir, node.Children);
         }
     }
 }
