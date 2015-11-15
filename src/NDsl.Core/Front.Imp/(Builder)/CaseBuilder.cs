6a273618620a6a602d038f2aea75874a615f93fa
Jerome <jeromerg@gmx.net>
2015-11-15 23:32:31 +0100

    added missing file

---
+++ src/NDsl.Core/Front.Imp/(Builder)/CaseBuilder.cs
@@ -0,0 +1,49 @@
+using System;
+using JetBrains.Annotations;
+using NDsl.Back.Api.Book;
+using NDsl.Back.Api.Builder;
+using NDsl.Back.Api.RecPlay;
+using NDsl.Back.Api.Util;
+using NDsl.Front.Api;
+
+namespace NDsl.Front.Imp
+{
+    public class Builder : ArtefactImp<IBuilderModel>, IBuilder, IBuilderModel
+    {
+        [NotNull] private readonly ITokenStream mTokenStream;
+        private RecPlayMode mRecPlayMode;
+
+        public Builder([NotNull] ITokenStream tokenStream, [NotNull] IServiceSet<IBuilderModel> services)
+            : base(services)
+        {
+            if (tokenStream == null) throw new ArgumentNullException("tokenStream");
+            mTokenStream = tokenStream;
+        }
+
+        public override IBuilderModel Model
+        {
+            get { return this; }
+        }
+
+        #region IBuilderModel
+
+        public ITokenStream TokenStream
+        {
+            get { return mTokenStream; }
+        }
+
+        public RecPlayMode RecPlayMode
+        {
+            get { return mRecPlayMode; }
+            set
+            {
+                if (mRecPlayMode == RecPlayMode.Recorded)
+                    throw new IndexOutOfRangeException("Recorded state is the initial state and cannot be set (should never happen)");
+
+                mRecPlayMode = value;
+            }
+        }
+
+        #endregion
+    }
+}
