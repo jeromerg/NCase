19d904b0aea3d9664587da1c0a3614f9ca02d15d
Jerome <jeromerg@gmx.net>
2016-01-14 23:36:56 +0100

    NUtil documentation

--- src/NUtil.Doc/Readme.cs
+++ src/NUtil.Doc/Readme.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
 using JetBrains.Annotations;
 using NDocUtil;
 using NUnit.Framework;
+using NUtil.Linq;
+using NUtil.Math.Combinatorics.Pairwise;
 
-namespace NCaseFramework.Doc
+namespace NUtil.Doc
 {
     [TestFixture]
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
     public class Readme
     {
         // ReSharper disable once InconsistentNaming
@@ -23,15 +29,89 @@ namespace NCaseFramework.Doc
         }
 
         [Test]
-        public void NCaseTree()
+        public void PairwiseGenerator()
         {
-            //# NCaseTree
-            //#
+            int i = 0;
 
-            docu.BeginRecordConsole("Visualize_Def_Console");
+            docu.BeginRecordConsole("PairwiseGenerator_Console");
+            //# PairwiseGenerator
+            var setCardinals = new int[] {3, 2, 2};
+            IEnumerable<int[]> tuples = new PairwiseGenerator().Generate(setCardinals);
+            foreach (int[] t in tuples)
+                Console.WriteLine("Tuple #{0}:  {1}, {2}, {3}", i++, t[0], t[1], t[2]);
+            //#
             docu.StopRecordConsole();
+
         }
 
+        void SendToServer(int i) { }
+        
+        [Test]
+        public void LinqForEachExtensions()
+        {
+            {
+                docu.BeginRecordConsole("LinqForEachExtensions1_Console");
+                //# LinqForEachExtensions1
+                var set = Enumerable.Range(0, 10);
+                set.ForEach(v => Console.Write(v));
+                //#
+                docu.StopRecordConsole();
+            }
+
+            Console.WriteLine();
+
+            {
+                docu.BeginRecordConsole("LinqForEachExtensions2_Console");
+                //# LinqForEachExtensions2
+                var set = Enumerable.Range(0, 10);
+                set.ForEach(v => Console.Write(v), () => Console.Write(", "));
+                //#
+                docu.StopRecordConsole();
+            }
+
+            Console.WriteLine();
+
+            {
+                //# LinqForEachExtensions3
+                var set = Enumerable.Range(0, 10);
+                set.ForEach(v => SendToServer(v), () => Thread.Sleep(10));
+                //#
+            }
+
+        }
+
+        [Test]
+        public void QuadraticExtensions()
+        {
+            docu.BeginRecordConsole("QuadraticExtensions_Console");
+            //# QuadraticExtensions
+            var set1 = new [] {"a", "b"};
+            var set2 = new [] {0, 1};
+
+            var product = set1.CartesianProduct(set2, (s1, s2) => new { s1, s2 });
+            
+            foreach (var pair in product)
+                Console.WriteLine("({0}, {1})", pair.s1, pair.s2);
+            //#
+            docu.StopRecordConsole();
+
+        }
+
+        [Test]
+        public void TriangularProductWithoutDiagonal()
+        {
+            docu.BeginRecordConsole("TriangularProductWithoutDiagonal_Console");
+            //# TriangularProductWithoutDiagonal
+            var set1 = new [] {0, 1, 2};
+
+            var product = set1.TriangularProductWithoutDiagonal((s1, s2) => new { s1, s2 });
+            
+            foreach (var pair in product)
+                Console.WriteLine("({0}, {1})", pair.s1, pair.s2);
+            //#
+            docu.StopRecordConsole();
+
+        }
     }
 
 }
--- src/NUtil.Doc/Readme.md
+++ src/NUtil.Doc/Readme.md
@@ -21,9 +21,109 @@ Content
 
 ### Pairwise Generator
 
+Considering a list of discrete and finite sets, the `PairwiseGenerator ` generates a list of tuples ensuring that all pairs between all sets pairwise appear at least once. 
+
+<!--# PairwiseGenerator -->
+```C#
+var setCardinals = new int[] {3, 2, 2};
+IEnumerable<int[]> tuples = new PairwiseGenerator().Generate(setCardinals);
+foreach (int[] t in tuples)
+    Console.WriteLine("Tuple #{0}:  {1}, {2}, {3}", i++, t[0], t[1], t[2]);
+```
+
+Console:
+
+<!--# PairwiseGenerator_Console -->
+```
+Tuple #0:  0, 0, 0
+Tuple #1:  0, 1, 1
+Tuple #2:  1, 0, 0
+Tuple #3:  1, 1, 1
+Tuple #4:  2, 0, 0
+Tuple #5:  2, 1, 1
+Tuple #6:  0, 0, 1
+Tuple #7:  0, 1, 0
+```
+
+This pairwise generator is used in [NCase] as an alternative to the default cartesian product, in order to reduce the amount of generated test cases. More about pairwise testing [here][pair].
+
 ### Linq ForEach
 
+Every Util framework re-implement the following ForEach extension method:
+
+<!--# LinqForEachExtensions1 -->
+```C#
+var set = Enumerable.Range(0, 10);
+set.ForEach(v => Console.Write(v));
+```
+Console:
+<!--# LinqForEachExtensions1_Console -->
+```
+0123456789
+```
+
+The following overload is a little bit more interesting, as it enables placing an action between the processing of two items:
+
+<!--# LinqForEachExtensions2 -->
+```C#
+var set = Enumerable.Range(0, 10);
+set.ForEach(v => Console.Write(v), () => Console.Write(", "));
+```
+Console:
+<!--# LinqForEachExtensions2_Console -->
+```
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+```
+
+If you want to perform the previous aggregation you already have the Linq `Aggregate(...)` function and the `string.Join(...)` static method. The following use has no counterpart in the C# framework:
+
+<!--# LinqForEachExtensions3 -->
+```C#
+var set = Enumerable.Range(0, 10);
+set.ForEach(v => SendToServer(v), () => Thread.Sleep(10));
+```
+
 ### Linq Quadratic Processing
+
+#### CartesianProduct
+
+<!--# QuadraticExtensions -->
+```C#
+var set1 = new [] {"a", "b"};
+var set2 = new [] {0, 1};
+
+var product = set1.CartesianProduct(set2, (s1, s2) => new { s1, s2 });
+
+foreach (var pair in product)
+    Console.WriteLine("({0}, {1})", pair.s1, pair.s2);
+```
+Console:
+<!--# QuadraticExtensions_Console -->
+```
+(a, 0)
+(a, 1)
+(b, 0)
+(b, 1)
+```
+
+#### TriangularProductWithoutDiagonal
+
+<!--# TriangularProductWithoutDiagonal -->
+```C#
+var set1 = new [] {0, 1, 2};
+
+var product = set1.TriangularProductWithoutDiagonal((s1, s2) => new { s1, s2 });
+
+foreach (var pair in product)
+    Console.WriteLine("({0}, {1})", pair.s1, pair.s2);
+```
+Console:
+<!--# TriangularProductWithoutDiagonal_Console -->
+```
+(0, 1)
+(0, 2)
+(1, 2)
+```
 
 ### Chained Dictionaries Processing
 
