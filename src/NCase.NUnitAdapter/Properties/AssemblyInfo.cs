82bc333d44b4411ae3f6db6f0c41bf8f168608a4
jglasson <jglasson.ext@autoscout24.com>
2016-02-01 13:04:15 +0100

    Homogenized wording in XUnit and NUnit adapters

--- src/NCase.NUnitAdapter/Properties/AssemblyInfo.cs
+++ src/NCase.NUnitAdapter/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NCase Nunit Adapter")]
+[assembly: AssemblyTitle("NCase Adapter for Nunit")]
 [assembly: AssemblyDescription("NCase: Define, Combine, Visualize and Replay hundreds of test cases with a few lines of code")]
 [assembly: ComVisible(false)]
 [assembly: Guid("66100de8-0d6c-4281-90bb-1ac8ed179fa4")]
